9a07474fa743b0fa828d97450f99e8f9b808c43d
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-10-06 19:05:07 -0400

    validation error messages showing html page

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,8 @@
 $().ready(function() {
   const limit = 140;
   //when entering input to textarea
-  $("#tweet-text").on("keyup", function() {
+  $("#tweet-text").on("input", function() {
+    $(".error").hide();
     let value = $(this).val().length;
     //treverse through the dom to get counter class
     let counter = $(this).closest('.tweet-form').find('.counter')
